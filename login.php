54d75698666c6f9d1d7893ad338a3b60a7235fa7
Luis A. Almaguer Garza <almaguer@gmail.com>
2024-11-01 16:58:19 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 
-$hardCodedUsername = 'Jorge';
-$hardCodedPassword = 'afccontadores24'; // Cambia esto por la contraseña que desees
+$hardCodedUsername = '';
+$hardCodedPassword = ''; // Cambia esto por la contraseña que desees
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = $_POST['username'] ?? 'Jorge';
-    $password = $_POST['password'] ?? 'afccontadores24';
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
 
     if ($username === $hardCodedUsername && $password === $hardCodedPassword) {
         $_SESSION['user_id'] = 1; // Puedes usar cualquier ID que desees
